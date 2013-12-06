6a7c42dd6793b17b740576223de5abf74216901e
brutalhonesty <sparky1010@gmail.com>
2013-12-06 14:18:39 -0700

    Added if-modified-since header

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var api = {};
 api.boards = function(cb) {
 	var uri = [baseUrl, "boards.json"].join("/");
 
-	request(uri, {json:true}, function(err, res, body){
+	request(uri, {json:true, headers: {'if-modified-since': (new Date()).toUTCString()}}, function(err, res, body){
 		if (err) return cb(err);
 		cb(null, body.boards);
 	});
@@ -25,7 +25,7 @@ api.board = function(board) {
 	subapi.catalog = function(cb) {
 		var uri = [baseUrl, board, "catalog.json"].join("/");
 
-		request(uri, {json:true}, function(err, res, body){
+		request(uri, {json:true, headers: {'if-modified-since': (new Date()).toUTCString()}}, function(err, res, body){
 			if (err) return cb(err);
 			cb(null, body);
 		});
@@ -36,7 +36,7 @@ api.board = function(board) {
 	subapi.threads = function(cb) {
 		var uri = [baseUrl, board, "threads.json"].join("/");
 
-		request(uri, {json:true}, function(err, res, body){
+		request(uri, {json:true, headers: {'if-modified-since': (new Date()).toUTCString()}}, function(err, res, body){
 			if (err) return cb(err);
 			cb(null, body);
 		});
@@ -47,7 +47,7 @@ api.board = function(board) {
 	subapi.page = function(num, cb) {
 		var uri = [baseUrl, board, num+".json"].join("/");
 
-		request(uri, {json:true}, function(err, res, body){
+		request(uri, {json:true, headers: {'if-modified-since': (new Date()).toUTCString()}}, function(err, res, body){
 			if (err) return cb(err);
 			cb(null, body.threads);
 		});
@@ -58,7 +58,7 @@ api.board = function(board) {
 	subapi.thread = function(num, cb) {
 		var uri = [baseUrl, board, "res", num+".json"].join("/");
 
-		request(uri, {json:true}, function(err, res, body){
+		request(uri, {json:true, headers: {'if-modified-since': (new Date()).toUTCString()}}, function(err, res, body){
 			if (err) return cb(err);
 			cb(null, body.posts);
 		});
